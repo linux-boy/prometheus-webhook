a2185a264dd9867185242eb4648c78c180d5e48e
pausky <pausky@126.com>
2019-07-30 13:48:01 +0800

    add flink

--- config/setting.py
+++ config/setting.py
@@ -8,3 +8,4 @@ import os
 conf = configparser.ConfigParser()
 conf.read(os.path.split(os.path.realpath(__file__))[0] + '/setting.ini')
 g_dingTalkHttpApi = conf.get('dingtalk', 'api')
+g_flaskPort = conf.get('flask', 'port')
---
+++ manage.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+import json
+from config import setting
+from flask import templating
+from dingtalk import messagebody
+from config import setting
+app = Flask(__name__)
+
+@app.route("/dtwebhook", methods=['POST'])
+def sendMessageToDingTalk():
+    d_data = json.loads(request.data)
+
+
+
+
+def after_request(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT,GET,POST,DELETE'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type,Authorization'
+    return response
+
+if __name__ == '__main__':
+    app.after_request(after_request)
+    app.run(port=setting.g_flaskPort, debug=False)
