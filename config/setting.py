98628e389eacacc53eb5f9e31ed1bf83023195fc
pausky <pausky@126.com>
2019-07-30 11:18:41 +0800

    add file

--- config/setting.py
+++ config/setting.py
@@ -7,4 +7,4 @@ import os
 # def getConfig(section, key):
 conf = configparser.ConfigParser()
 conf.read(os.path.split(os.path.realpath(__file__))[0] + '/setting.ini')
-    # return conf.get(section, key)
+g_dingTalkHttpApi = conf.get('dingtalk', 'api')
