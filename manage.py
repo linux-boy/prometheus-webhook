6e2c43b375211a52e0d3bca54d42607094cc2cc9
pausky <pausky@126.com>
2019-08-02 16:39:49 +0800

    add function

--- manage.py
+++ manage.py
@@ -17,7 +17,7 @@ def sendMessageToDingTalk():
     for b_text in d_data['alerts']:
         s_startsAt = b_text['startsAt']
         s_endsAt = b_text['endsAt']
-        s_title = s_title +'['+ b_text['labels']['cluster']+']'+'('+b_text['labels']['generatorURL']+')'
+        s_title = s_title +'['+ b_text['labels']['cluster']+']'+'('+b_text['generatorURL']+')'
 
     d_body = messagebody.dingMarkDown(s_title=s_title, s_content=s_startsAt+'\n'+s_endsAt+'\n'+s_text)
     # print(d_body)
