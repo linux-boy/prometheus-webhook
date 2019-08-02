1113f55f65eb3b41833b4ba9e72002361d9c827f
pausky <pausky@126.com>
2019-08-02 16:09:43 +0800

    add function

--- dingtalk/sendmessage.py
+++ dingtalk/sendmessage.py
@@ -4,9 +4,8 @@ from config import setting
 import json
 
 def sendDingTalk(d_message):
-    d_dtmessage = messagebody.dingMarkDown('test', 'test', ['18898692781'],'test')
     head = {"Content-Type": "application/json"}
-    data = requests.post(url=setting.g_dingTalkHttpApi,data=json.dumps(d_dtmessage), headers=head)
+    data = requests.post(url=setting.g_dingTalkHttpApi,data=json.dumps(d_message), headers=head)
 
 if __name__ == '__main__':
     sendDingTalk('test')
--- manage.py
+++ manage.py
@@ -3,12 +3,22 @@ import json
 from config import setting
 from flask import templating
 from dingtalk import messagebody
+from dingtalk import sendmessage
 from config import setting
 app = Flask(__name__)
 
 @app.route("/dtwebhook", methods=['POST'])
 def sendMessageToDingTalk():
-    d_data = json.loads(request.data)
+    d_data = json.loads(request.data.decode())
+    s_title = d_data['annotations']['description']
+    s_startsAt = d_data['startsAt']
+    s_endsAt = d_data['endsAt']
+    s_text = ''
+    for b_text in d_data['alerts']:
+        s_title = s_title +'['+ b_text['labels']['cluster']+']'+'('+b_text['labels']['generatorURL']+')'
+    d_body = messagebody.dingMarkDown(s_title=s_title, s_content=s_startsAt+'\n'+s_endsAt+'\n'+s_text)
+    sendmessage(d_body)
+    return '200'
     
 
 
